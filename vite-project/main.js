369159c424dd870ad65b6d7346fde9e50105ba01
Yolanda <yeckdemies@gmail.com>
2024-04-15 19:40:23 +0200

    Ingredientes

--- vite-project/main.js
+++ vite-project/main.js
@@ -6,36 +6,42 @@ const products = [
     price: 3.35,
     description:
       'Bebida Baja en Calorías con Extracto de Café Verde y Refrescante Sabor a Lima.',
+    ingredientes: ['Te verde', 'Leche'],
     image: 'assets/images/Classic-Chai-Tea-Latte.jpeg'
   },
   {
     name: 'ICED Chai Tea Latte',
     price: 4.5,
     description: 'Té negro chai, hielo y leche o bebida vegetal',
+    ingredientes: ['Te negro', 'Leche', 'Bebida vegetal'],
     image: 'assets/images/SBX-UK-MOP-IcedChaiTeaLatte.jpg'
   },
   {
     name: 'Matcha tea latte',
     price: 5.5,
     description: 'Leche o bebida vegetal con matcha con un sabor único',
+    ingredientes: ['Te matcha', 'Leche', 'Bebida vegetal'],
     image: 'assets/images/Matcha-Green-Tea-Latte.jpeg'
   },
   {
     name: 'Iced Matcha tea latte',
     price: 6.5,
     description: 'Leche  o bebida vegetal, hielo y matcha con un sabor único',
+    ingredientes: ['Te matcha', 'Leche', 'Bebida vegetal', 'Hielo'],
     image: 'assets/images/Iced-Matcha-Green-Tea-Latte.jpeg'
   },
   {
     name: 'Iced lemon',
     price: 6.5,
     description: 'Bebida refrescante de limón hielo y limón liofilizado',
+    ingredientes: ['Limón', 'hielo'],
     image: 'assets/images/Iced-Green-Tea-Lemonade.jpeg'
   },
   {
     name: 'Teavana® Peach Citrus Green Tea Lemonade',
     price: 6.5,
-    description: '',
+    description: 'Bebida refrescante de cítricos y te verde',
+    ingredientes: ['Cítricos', 'Te verde'],
     image: 'assets/images/SBX-UK-MOP-PeachIcedTea.jpg'
   },
   {
@@ -43,6 +49,7 @@ const products = [
     price: 6.5,
     description:
       'Té Verde Frio con Melocotón y Limonada. Bien Agitado en Coctelera y Servido con Hielo',
+    ingredientes: ['Te verde', 'Limonada', 'Melocotón'],
     image: 'assets/images/SBX-UK-MOP-IcedGreenTeaLemonade.jpeg'
   },
   {
@@ -50,6 +57,7 @@ const products = [
     price: 6.5,
     description:
       'Té Negro Frio con Limonada. Bien Agitado en Coctelera y Servido con Hielo',
+    ingredientes: ['Limonada', 'Te negro'],
     image: 'assets/images/Iced-Black-Tea-Lemonade.jpeg'
   },
   {
@@ -57,36 +65,42 @@ const products = [
     price: 6.5,
     description:
       'Infusión de Hibiscus Frio con Limonada. Bien Agitado en Coctelera y Servido con Hielo',
+    ingredientes: ['Hibiscus', 'Limonada'],
     image: 'assets/images/Iced-Hibiscus-Tea-Lemonade.jpeg'
   },
   {
     name: 'Teavana® Green Tea',
     price: 6.5,
     description: 'Té Verde Frio Servido con Hielo',
+    ingredientes: ['Te verde', 'Hielo'],
     image: 'assets/images/Iced-Green-Tea.jpeg'
   },
   {
     name: 'Teavana® Black Tea',
     price: 6.5,
     description: 'Té Negro Frio Servido con Hielo',
+    ingredientes: ['Te negro', 'Hielo'],
     image: 'assets/images/Iced-Black-Tea.jpeg'
   },
   {
     name: 'Teavana® Hibiscus Infusion',
     price: 6.5,
     description: 'Infusión de Hibiscus con Hielo.',
+    ingredientes: ['Hibiscus', 'Hielo'],
     image: 'assets/images/SBX-UK-MOP-HibiscusBlend.jpeg'
   },
   {
     name: 'English Breakfast',
     price: 6.5,
     description: 'Té negro con cuerpo y ricos matices a roble y malta.',
+    ingredientes: ['Te negro'],
     image: 'assets/images/English-Breakfast-Tea.jpeg'
   },
   {
     name: 'Earl Grey',
     price: 6.5,
     description: 'Te suave con aromáticos matices a bergamota y lavanda.',
+    ingredientes: ['Te aromático', 'bergamota', 'Lavanda'],
     image: 'assets/images/Earl-Grey-Tea_0.jpeg'
   },
   {
@@ -94,6 +108,7 @@ const products = [
     price: 6.5,
     description:
       'Infusión de canela picante, aroma de jengibre y toques de cardamomo.',
+    ingredientes: ['Canela', 'Jengibre', 'Cardamomo'],
     image: 'assets/images/Chai-Tea.jpeg'
   },
   {
@@ -101,12 +116,14 @@ const products = [
     price: 6.5,
     description:
       'Mezcla equilibrada de té verde con notas de hierbaluisa, hierba de limón y una pizca de menta.',
+    ingredientes: ['Te verde', 'Hierbaluisa', 'Hierba de Limón', 'Menta'],
     image: 'assets/images/Mint-Citrus-Tea.jpeg'
   },
   {
     name: 'Mint Blend',
     price: 6.5,
     description: 'Hierbabuena, menta y un toque de hierbaluisa (sin cafeína)',
+    ingredientes: ['Hierbabuena', 'Hierbaluisa', 'Menta'],
     image: 'assets/images/Mint-Herbal-Blend-Tea.jpeg'
   },
   {
@@ -114,13 +131,15 @@ const products = [
     price: 6.5,
     description:
       'Té verde puro de origen único con un sutil acabado ahumado-dulce.',
+    ingredientes: ['Te verde'],
     image: 'assets/images/SBX-UK-MOP-EmperorsClouds&Mist.jpeg'
   },
   {
     name: 'Hibiscus',
     price: 6.5,
     description:
-      'Toques tropicales de papaya y mango con hibisco, hierba de limón y un toque de canela (sin cafeína)',
+      'Toques tropicales de papaya y mango con hibiscus, hierba de limón y un toque de canela (sin cafeína)',
+    ingredientes: ['Hibiscus', 'Papaya', 'Mango', 'Hierba de Limón', 'Canela'],
     image: 'assets/images/Hibiscus-Herbal-Blend-Tea.jpeg'
   },
   {
@@ -128,11 +147,10 @@ const products = [
     price: 6.5,
     description:
       'Té blanco con piña dulce, baya de acaí y un final floral a pétalos de rosa.',
+    ingredientes: ['Te blanco', 'Piña', 'Baya', 'Pétalos de rosa'],
     image: 'assets/images/Youthberry-Tea.jpeg'
   }
 ];
-
-//añadir a .cafeGrid
 
 const sectionCafeGrid = document.querySelector('.cafeGrid');
 
@@ -158,7 +176,11 @@ const generarGrid = (productList) => {
     p.className = 'product-text';
     p.innerText = product.description;
 
-    div.append(img, h2, p);
+    const price = document.createElement('h2');
+    price.className = 'product-title';
+    price.innerText = `Precio: ${product.price} €`;
+
+    div.append(img, h2, p, price);
 
     article.append(div);
     sectionCafeGrid.append(article);
@@ -166,3 +188,31 @@ const generarGrid = (productList) => {
 };
 
 generarGrid(products);
+
+const removeDuplicates = (param) => {
+  for (let i = 0; i < param.length; i++) {
+    const primerPuntero = param[i];
+    // empezamos en la siguiente a la i para no comprobar la misma con la misma
+    for (let j = i + 1; j < param.length; j++) {
+      const segundoPuntero = param[j];
+      // hemos encontrado un duplicado!
+      if (primerPuntero === segundoPuntero) {
+        // método splice para eliminar uno
+        param.splice(j, 1);
+        // damos un pasito atrás para no saltarnos ninguna comprobación
+        j--;
+      }
+    }
+  }
+};
+
+const extraerIngredientes = (productList) => {
+  const ingredientes = [];
+  productList.forEach((product) => {
+    for (let i = 0; i < product.ingredientes.length; i++) {
+      ingredientes.push(product.ingredientes[i]);
+    }
+  });
+  removeDuplicates(ingredientes);
+  return ingredientes;
+};
